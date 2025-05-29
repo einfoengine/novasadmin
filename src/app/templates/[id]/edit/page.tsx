ddea6c695f4fb3cff10a1c0591faf8f113389fa5
Mohammad Shanewaz Al Maruf <einfoengine@gmail.com>
2025-05-30 02:19:22 +0600

    Edit template page - primarily done

--- src/app/templates/[id]/edit/page.tsx
+++ src/app/templates/[id]/edit/page.tsx
@@ -3,6 +3,7 @@
 import React from 'react';
 import CreateTemplate from '@/components/CreateTemplate';
 
-export default function EditTemplatePage({ params }: { params: { id: string } }) {
-  return <CreateTemplate mode="edit" templateId={params.id} />;
+export default function EditTemplatePage({ params }: { params: Promise<{ id: string }> }) {
+  const resolvedParams = React.use(params);
+  return <CreateTemplate mode="edit" templateId={resolvedParams.id} />;
 } 
